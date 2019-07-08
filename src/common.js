d0330aef87100bbdbfcce5167a79d6cdfa5846ce
WalkerShan <403080318@qq.com>
2019-07-09 00:36:03 +0800

    修改缩进

--- src/common.js
+++ src/common.js
@@ -4,16 +4,16 @@ import Header from './components/Header'
 import './style/common.less'
 export default class Common extends React.Component {
 
-    render() {
-        return (
-            <div>
-                <Row className="simple-page">
-                    <Header menuType="second" />
-                </Row>
-                <Row className="content">
-                    {this.props.children}
-                </Row>
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <Row className="simple-page">
+          <Header menuType="second" />
+        </Row>
+        <Row className="content">
+          {this.props.children}
+        </Row>
+      </div>
+    );
+  }
 }
